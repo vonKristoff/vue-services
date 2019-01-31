e3dc89620398c6202a7818f237cee99071fbd011
vonKristoff <mrjcnicolas@gmail.com>
2019-01-31 16:25:16 +0000

    update core files to integrate example

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
+import loginForm from './components/login-form/vm'
 export default {
 	el: "#root",
-	components: {}
+	components: { loginForm },
+    created() {
+        ApiService.init(process.env.VUE_APP_ROOT_API)
+
+        // If token exists set header
+        if(TokenService.getToken()) ApiService.setHeader()
+    }
 }
